b2050b4479a7bc6e1a56d43813503ae43a631e2f
Mona Lisa <zdovile123@gmail.com>
2023-04-02 16:08:36 +0300

    final

--- src/components/ImageWithText.js
+++ src/components/ImageWithText.js
@@ -9,10 +9,11 @@ const ImageWithText = () => {
         <div className="text-wrap">
           <h3>High quality, long lasting furniture</h3>
           <p>
-            Fugiat consequat voluptate ea veniam ipsum veniam anim laborum nisi
-            sunt. Minim deserunt laboris enim ullamco consectetur cupidatat
-            tempor reprehenderit laboris dolore pariatur culpa culpa consequat.
-            Lorem amet do enim laborum commodo dolore occaecat.
+            We believe that furniture should be more than just a functional item
+            in your home, it should also be a work of art that brings warmth and
+            character to your space. At our shop, we are committed to promoting
+            sustainable living and providing our customers with quality products
+            that are not only beautiful but also eco-friendly.
           </p>
           <Link to="/about">Read more</Link>
         </div>
